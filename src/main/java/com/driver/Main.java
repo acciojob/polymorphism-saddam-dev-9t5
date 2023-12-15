fe6ed001e106a1845c57b4c5124aa7b47422190c
saddam-dev-9t5 <saddam.dev.9t5@gmail.com>
2023-12-15 22:22:18 +0530

    submit code by saddam.dev.9t5

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,25 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args) {
+        Product p = new Product();
+        int ans = p.product(10, 20);
+        int ans2 = p.product(10, 20, 30);
+        double ans3 = p.product(10.0, 20.0);
+        System.out.println(ans + " " + ans2 + " " + ans3);
+    }
+
+    static class Product {
+        public int product(int x, int y) {
+            return x*y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
 }
