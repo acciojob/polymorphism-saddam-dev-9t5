af814c81c1fd549755212c7b747b70fafba0c213
saddam-dev-9t5 <saddam.dev.9t5@gmail.com>
2023-12-15 22:26:29 +0530

    resolved public class issues generated by test cases

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.println(ans + " " + ans2 + " " + ans3);
     }
 
-    static class Product {
+    public static class Product {
         public int product(int x, int y) {
             return x*y;
         }
